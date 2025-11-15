650f31f3a2fedc07d565a352321dec6dea5a6e2d
Tadiyos <tadi@gmail.com>
2025-11-15 06:24:22 +0300

    resolving disconnecting issue

--- app.py
+++ app.py
@@ -70,13 +70,6 @@ async def websocket_endpoint(websocket: WebSocket):
 
 if __name__ == "__main__":
     logger.info("Starting Web Terminal server...")
-        port = int(os.getenv("PORT", 8000))
-        logger.info(f"Access it at http://0.0.0.0:{port}")
-        uvicorn.run("app:app", host="0.0.0.0", port=port, reload=True)
-
-
-if __name__ == "__main__":
-    logger.info("Starting Web Terminal server...")
-    logger.info("Access it at http://0.0.0.0:8000")
-    
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.getenv("PORT", 8000))
+    logger.info(f"Access it at http://0.0.0.0:{port}")
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=True)
